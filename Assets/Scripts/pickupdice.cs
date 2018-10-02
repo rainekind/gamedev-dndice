b47f9ac5768cd7076c5ad19678335fb568b12885
Emma Raine Stone <emmaleestone@gmail.com>
2018-10-02 13:36:41 -0400

    Pickup dice code still not working.

---
+++ Assets/Scripts/pickupdice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupdice : MonoBehaviour {
+
+public Collider bodycollider;
+
+	// Use this for initialization
+	void Start () {
+	
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider bodycollider)
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Destroy(GameObject.FindGameObjectWithTag("dice"));
+		}
+	}
+}
